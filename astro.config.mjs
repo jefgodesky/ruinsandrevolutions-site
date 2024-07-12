59fff2b1ca3ae28a6664c093920127f30101a6c1
Jason Godesky <jefgodesky@gmail.com>
2024-07-12 16:09:09 -0400

    Enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ruinsandrevolutions.com',
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: "server",
+  adapter: netlify()
 });
