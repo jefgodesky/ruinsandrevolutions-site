adb3841dbd0d2408f8076148299becc3a0f901e8
Jason Godesky <jefgodesky@gmail.com>
2024-07-14 13:28:51 -0400

    Add new component: MarkdownContent

---
+++ src/utils/parse-markdown.ts
@@ -0,0 +1,16 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import rehypeRaw from 'rehype-raw'
+
+export async function parseMarkdown(markdown: string): Promise<string> {
+  const html = await unified()
+    .use(remarkParse)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
+    .process(markdown)
+
+  return String(html)
+}
