72fef6ae1130e7bb307addd201319bf816a4f68d
Jason Godesky <jefgodesky@gmail.com>
2024-07-21 14:05:36 -0400

    Add breadcrumbs to world listings

--- src/utils/get-auth.ts
+++ src/utils/get-auth.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken'
 const getAuth = (astro: AstroGlobal): { apiKey: string, username: string } | null => {
   const token = astro.cookies.get('auth')?.value ?? ''
   try {
+    // @ts-ignore
     const data = jwt.verify(token, import.meta.env.JWT_SECRET ?? '') as jwt.JwtPayload
     return { apiKey: data.apiKey ?? '', username: data.username ?? '' }
   } catch {
