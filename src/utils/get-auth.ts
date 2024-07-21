0d13ae27ae382f13be4c4b139c8b0554d098230a
Jason Godesky <jefgodesky@gmail.com>
2024-07-21 13:28:29 -0400

    Use JWT for authentication

---
+++ src/utils/get-auth.ts
@@ -0,0 +1,14 @@
+import type { AstroGlobal } from 'astro'
+import jwt from 'jsonwebtoken'
+
+const getAuth = (astro: AstroGlobal): { apiKey: string, username: string } | null => {
+  const token = astro.cookies.get('auth')?.value ?? ''
+  try {
+    const data = jwt.verify(token, import.meta.env.JWT_SECRET ?? '') as jwt.JwtPayload
+    return { apiKey: data.apiKey ?? '', username: data.username ?? '' }
+  } catch {
+    return null
+  }
+}
+
+export default getAuth
