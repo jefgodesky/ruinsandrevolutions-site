ada36f0e41fa3c0f861daf66c544b9fbe731a798
Jason Godesky <jefgodesky@gmail.com>
2024-08-02 21:24:38 -0400

    Random Tables

--- src/utils/parse-markdown.ts
+++ src/utils/parse-markdown.ts
@@ -3,14 +3,30 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 import rehypeRaw from 'rehype-raw'
+import { visit } from 'unist-util-visit'
 
-export async function parseMarkdown(markdown: string): Promise<string> {
-  const html = await unified()
+const removeParagraphs = () => {
+  return (tree: any) => {
+    visit(tree, 'element', (node, index, parent) => {
+      if (node.tagName === 'p') {
+        if (parent && parent.children && Array.isArray(parent.children)) {
+          parent.children.splice(index, 1, ...node.children)
+        }
+      }
+    })
+  }
+}
+
+const parseMarkdown = async (markdown: string, noParagraph: boolean = false): Promise<string> => {
+  const processor = unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
     .use(rehypeStringify)
-    .process(markdown)
 
+  if (noParagraph) processor.use(removeParagraphs)
+  const html = await processor.process(markdown)
   return String(html)
 }
+
+export default parseMarkdown
