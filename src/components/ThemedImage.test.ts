64bad3858691a5df773782a0f20f5cb8a8ee5f7b
Jason Godesky <jefgodesky@gmail.com>
2025-02-16 17:42:44 -0500

    Cool dragon head on homepage bg
    Involves figuring out our ThemedImage component while we're at it.

--- src/components/ThemedImage.test.ts
+++ src/components/ThemedImage.test.ts
@@ -1,72 +1,20 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container'
-import { expect, describe, it, afterEach, afterAll } from 'vitest'
-import MatchMediaMock from 'vitest-matchmedia-mock'
+import { expect, describe, it } from 'vitest'
 import ThemedImage from './ThemedImage.astro'
 
 describe('ThemedImage', () => {
-  let matchMediaMock = new MatchMediaMock();
-  const props = {
-    light: '/light.png',
-    dark: '/dark.png',
-    alt: 'Test image',
-    height: '100px',
-    width: '100px',
-    classes: 'custom'
-  }
-
-  afterEach(() => {
-    localStorage.clear()
-    matchMediaMock.clear()
-  })
-
-  afterAll(() => {
-    matchMediaMock.destroy()
-  })
-
-  const expectContains = (result: string, contains: string, not: string): void => {
-    expect(result).toContain(contains)
-    expect(result).not.toContain(not)
-  }
-
-  const expectLight = (result: string): void => {
-    expectContains(result, `src="${props.light}"`, `src="${props.dark}"`)
-  }
-
-  const expectDark = (result: string): void => {
-    expectContains(result, `src="${props.dark}"`, `src="${props.light}"`)
-  }
-
-  it('renders the light image when localStorage is set to "light"', async () => {
-    localStorage.setItem('theme', 'light')
+  it('renders light and dark versions in a picture tag', async () => {
     const container = await AstroContainer.create()
-    const result = await container.renderToString(ThemedImage, { props })
-    expectLight(result)
-  })
-
-  it('renders the dark image when localStorage is set to "dark"', async () => {
-    localStorage.setItem('theme', 'dark')
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(ThemedImage, { props })
-    expectDark(result)
-  })
-
-  it('renders the light image when no stored theme but prefers-color-scheme is light', async () => {
-    matchMediaMock.useMediaQuery('(prefers-color-scheme: light)')
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(ThemedImage, { props })
-    expectLight(result)
-  })
-
-  it('renders the dark image when no stored theme but prefers-color-scheme is dark', async () => {
-    matchMediaMock.useMediaQuery('(prefers-color-scheme: dark)')
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(ThemedImage, { props })
-    expectDark(result)
-  })
-
-  it('renders the light image when no stored theme and matchMedia returns nothing', async () => {
-    const container = await AstroContainer.create()
-    const result = await container.renderToString(ThemedImage, { props })
-    expectLight(result)
+    const result = await container.renderToString(ThemedImage, {
+      props: {
+        light: '/light.png',
+        dark: '/dark.png',
+        alt: 'Alt text',
+        height: 3000,
+        width: 3000,
+        classes: ['test']
+      }
+    })
+    expect(result).toBe('<picture data-astro-source-file="/Users/jefgodesky/Sites/ruinsandrevolutions/site/src/components/ThemedImage.astro" data-astro-source-loc="35:10"> <source media="(prefers-color-scheme: dark)" srcset="/dark.png" data-astro-source-file="/Users/jefgodesky/Sites/ruinsandrevolutions/site/src/components/ThemedImage.astro" data-astro-source-loc="36:4"> <source media="(prefers-color-scheme: light)" srcset="/light.png" data-astro-source-file="/Users/jefgodesky/Sites/ruinsandrevolutions/site/src/components/ThemedImage.astro" data-astro-source-loc="37:4"> <img src="/light.png" alt="Alt text" height="3000" width="3000" class="themed-image test" data-astro-source-file="/Users/jefgodesky/Sites/ruinsandrevolutions/site/src/components/ThemedImage.astro" data-astro-source-loc="38:4"> </picture>')
   })
 })
