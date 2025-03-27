ab47e17f936515be4e4d0e03c7d343e72c6f1918
Jason Godesky <jefgodesky@gmail.com>
2025-03-27 14:12:42 -0400

    Set up Node adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 
-// https://astro.build/config
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  output: 'server',
+  site: isProd ? 'https://ruinsandrevolutions.com' : undefined,
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4321'),
+  },
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react(), mdx()]
 })
